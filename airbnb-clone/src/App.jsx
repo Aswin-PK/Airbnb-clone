7b2c936b85b8fff7ab72acc7c943560816774b5d
Aswin-PK <aswinpk7737@gmail.com>
2023-12-27 02:15:44 +0530

    added favicon, fontawesome icon cdn and made small changes

--- airbnb-clone/src/App.jsx
+++ airbnb-clone/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState } from 'react'
-import './App.css'
 import Navbar from './Components/Navbar/Navbar'
 import Categories from './Components/Categories/Categories'
 import RowCards from './Components/RowCards/RowCards'
+import Footer from './Components/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +11,8 @@ function App() {
     <div className=''>
       <Navbar />
       <Categories />
-      <RowCards imageUrl="https://a0.muscache.com/im/pictures/458e3fa3-3b67-4dc5-87b3-65178557429b.jpg?im_w=720"/>
+      <RowCards />
+      <Footer />
     </div>
   )
 }
